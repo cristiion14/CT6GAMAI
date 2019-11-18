4615f4f4d1da88ba2b1e194751a15a97dadb2a30
cristiion14 <cristi.ion1996@gmail.com>
2019-11-18 14:35:12 +0000

    Cleaned Iohanis's script

--- Unity Project/FSM/Assets/Scripts/Iohannis/Iohannis.cs
+++ Unity Project/FSM/Assets/Scripts/Iohannis/Iohannis.cs
@@ -9,25 +9,30 @@ public class Iohannis : MonoBehaviour {
     public bool isInChasingState;
     StateManager<Iohannis> fsm = new StateManager<Iohannis>();
 
+    [SerializeField]
+    float travelSpeed = 0.07f;
+
     public float health = 100f;
-    float speed = 2.5f;
 
     Vector3[] path;
    public int targetIndex;
-    // Use this for initialization
-    Patrol patrolState;
 
     float distance;
+
    public Transform target;
     public Transform[] patrolPoints;
+
     public float lookRad = 25f;
     public float shootDist = 20f;
    public float startTimeBtwShoots, timeBtwShoots;
     public GameObject bullet, bulletPoint;
+
     Grid grid;
    public GameObject gm;
+
   public  bool isFound, foundTarget;
     public int nr = 0, hasBeenFoundAtNr=0;  //the path nr and the nr which keeps track at what path nr has been found
+
     private void Awake()
     {
         grid = GetComponent<Grid>();
@@ -43,12 +48,6 @@ public class Iohannis : MonoBehaviour {
         agent = GetComponent<NavMeshAgent>();
         target = AgentManager.instance.enemy2.transform;
         timeBtwShoots = startTimeBtwShoots;
-       // PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
-        for (int i = 0; i < 3; i++)
-        {
-            patrolPoints[i] = gm.GetComponent<AgentManager>().patrolPoints[i].transform;
-        }
-        //grid = GetComponent<Grid>();
     }
 	public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
     {
@@ -79,23 +78,14 @@ public class Iohannis : MonoBehaviour {
                 }
                 currentWaypoint = path[targetIndex];
     //        Debug.Log("current waypoint is: " + currentWaypoint);
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, 0.07f);
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, travelSpeed);
             yield return null;
         }
        
     }
 	// Update is called once per frame
 	void Update () {
-        PointLocation();
           fsm.Execute();
-        //    ChasePlayer();
-        // isChased();
-      
-        targetFound();
-        FindTarget();
-      //  PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
-        //  FaceTarget();
-        //  Shoot();
 
     }
     public bool targetFound()
@@ -106,11 +96,7 @@ public class Iohannis : MonoBehaviour {
         else
             return false;
     }
-    public void FindTarget()
-    {
-     //   gm.GetComponentInChildren<Pathfinding>().StartFindPath(gameObject.transform.position, target.transform.position);
-
-    }
+   
    public void FaceTarget()
     {
         // Debug.Log("Face Target");
@@ -120,9 +106,7 @@ public class Iohannis : MonoBehaviour {
     }
 
     public void FacePatrolPoint(int patrolPoint)
-    {
-        // Debug.Log("Face Target");
-       
+    {  
             Vector3 direction = (patrolPoints[patrolPoint].transform.position - transform.position).normalized;
             Quaternion lookRot = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * 5);
@@ -134,32 +118,7 @@ public class Iohannis : MonoBehaviour {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, lookRad);
     }
-   public void ChasePlayer()
-    {
-     //  get the distance from player to enemy
-       distance = Vector3.Distance(target.position, transform.position);
-        if (distance <= lookRad)
-        {//if distance is less-----set destination
-            foundTarget = true;
 
-
-            agent.SetDestination(target.position);
-            agent.stoppingDistance = 75f;
-            FaceTarget();
-            Shoot();
-        }
-        //foundTarget = false;
-    }
-
-    void isChased()
-    {
-        if(isFound)
-        {
-            agent.speed += 5f;
-            agent.SetPath(new NavMeshPath());
-        }
-       
-    }
    public void Shoot()
     {
         if (timeBtwShoots <= 0)
@@ -171,23 +130,6 @@ public class Iohannis : MonoBehaviour {
         else
         {
             timeBtwShoots -= Time.deltaTime;
-        }
-    }
-    void PointLocation()
-    {
-        //check to see if the left mouse was pressed
-        if (Input.GetMouseButtonDown(0))
-        {
-            //get pos of the mouse
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);      //convert the position of the mouse into a ray that can be shooted anywhere in the scene and store it in a ray var
-            // var to store info about what the ray hits
-            RaycastHit hit;
-            //shoot ray and move agent just if the ray hits something
-            if (Physics.Raycast(ray, out hit))
-            {
-                //move agent
-                agent.SetDestination(hit.point);
-            }
         }
     }
 
